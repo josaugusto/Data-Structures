6cf22bf1b6125496a516740020dd9455c5e3b8e9
José Augusto <79456742+josaugusto@users.noreply.github.com>
2022-04-29 12:21:27 -0300

    Update main.c

--- listadinâmica/main.c
+++ listadinâmica/main.c
@@ -1,48 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no{
+typedef struct node{
     int info;
-    struct no *prox;
-}No;
+    struct node *next;
+}Node;
 
 typedef struct{
-    int nelementos;
-    No *head;
-}Lista;
+    int nelements;
+    Node *head;
+}List;
 
-Lista *cria_lista(void)
+List *new_list(void)
 {
-    Lista *l = (Lista*) malloc(sizeof(Lista));
+    List *l = (List*) malloc(sizeof(List));
     l->head = NULL;
-    l->nelementos = 0;
-    
+    l->nelements = 0;
 }
 
-void inserir_elemento(Lista *l, int elemento)
+void insert_firstelement(List *l, int element)
 {
-    if (l->nelementos == 0)
-    {
-        No *novo = (No*) malloc(sizeof(No));
-        novo->info = elemento;
-        novo->prox = NULL;
-        l->head = novo;
-        l->nelementos++;
-
-    }
-    else
-    {
-        No *novo = (No*) malloc(sizeof(No));
-        novo->info = elemento;
-        novo->prox;
-        
-    }
+    Node *new = (Node*) malloc(sizeof(Node));
+    new->info = element;
+    new->next = NULL;
+    l->head = new;
+    l->nelements++;
 }
 
+void insert_inbegin(List *l, int element)
+{
+   Node *aux = (Node*) malloc(sizeof(Node));
+   aux = l->head;
+   Node *new = (Node*) malloc(sizeof(Node));
+   new->info = element;
+   new->next = aux;
+   l->head = new;
+   l->nelements++;
+}
+
+void insert_inend(List *l, int element)
+{
+      Aluno *p = (Aluno*) malloc(sizeof(Aluno));
+        p = l->head;
+        while(p->prox != NULL) p = p->prox;
+        
+        Aluno *novo = (Aluno*) malloc(sizeof(Aluno));
+        strcpy(novo->nome, nome);
+        novo->prioridade = prioridade;
+        l->nalunos++;
+        p->prox = novo;
+}
 int main()
 {
-    Lista *lista = cria_lista();
-
+    
+    
 
     return 0;
-}   
+} 
