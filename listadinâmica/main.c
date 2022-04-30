2e19917ff4ce14d51b49ee65b6e5111c1a098854
José Augusto <79456742+josaugusto@users.noreply.github.com>
2022-04-30 17:01:11 -0300

    Update main.c

--- listadinâmica/main.c
+++ listadinâmica/main.c
@@ -10,7 +10,8 @@ typedef struct node{
 
 typedef struct{
     int nelements;
-    Node *head;
+    Node *head; // begin of list
+    Node *tail; // end of list
 }List;
 
 List *new_list(void)
@@ -18,59 +19,138 @@ List *new_list(void)
     List *l = (List*) malloc(sizeof(List));
     l->head = NULL;
     l->nelements = 0;
-}
-
-void insert_firstelement(List *l, int element)
-{
-    Node *new = (Node*) malloc(sizeof(Node));
-    new->info = element;
-    new->next = NULL;
-    l->head = new;
-    l->nelements++;
+    return l;
 }
 
 void insert_inbegin(List *l, int element)
 {
-   Node *aux = (Node*) malloc(sizeof(Node));
-   aux = l->head;
-   Node *new = (Node*) malloc(sizeof(Node));
-   new->info = element;
-   new->next = aux;
-   l->head = new;
-   l->nelements++;
+
+    Node *new = (Node*) malloc(sizeof(Node));
+    new->info = element;
+
+    if (l->head == NULL) 
+    {    
+        new->next = NULL;
+        l->head = new;
+        l->tail = new;
+    }
+    else 
+    {
+        new->next = l->head;
+        l->head = new;
+    }
+
+    l->nelements++;
 }
 
 void insert_inend(List *l, int element)
 {
-    Node *p = (Node*) malloc(sizeof(Node));
-    for (p = l->head; p != NULL; p = p->next);
-        
     Node *new = (Node*) malloc(sizeof(Node));
     new->info = element;
-    p->next = new;
+
+    if (l->head == NULL) 
+    {    
+        new->next = NULL;
+        l->head = new;
+        l->tail = new;
+    }
+    else
+    {
+        new->next == NULL;
+        l->tail->next = new;
+        l->tail = new;
+    }
+
     l->nelements++;
+}
+
+void remove_oneelement(List *l, int element)
+{
+    int r = 0;
+
+    if (l->head->info == element) // remove in begin
+    {
+        Node *aux = l->head;
+        l->head = aux->next;
+        free(aux); r++;
+    }
+    else
+    {
+        Node *p = l->head; 
+        Node *prev = NULL;
+    
+        while (p != NULL && r != 1)
+        {
+            if (p->info == element)
+            {
+                if (p->next == NULL) // remove in end of list
+                {
+                    free(p);
+                    prev->next = NULL;
+                    l->tail = prev; 
+                }
+                else // remove in middle of list
+                {
+                    prev->next = p->next;
+                    free(p); 
+                }
+                r++;
+                break;
+            }
+            prev = p;
+            p = p->next;
+        }
+    }
+    if (r == 1)
+    {
+        printf("Element deleted with sucess!\n");
+        l->nelements--;
+    }
+    else printf("Element not found!\n");
+    
 }
 
 void print(List *l)
 {
-    Node *p = (Node*) malloc(sizeof(Node));
+    Node *p = l->head;
+
+    if (l->nelements == 0)
+    {
+        printf("The List is empty\n");
+        return;
+    }
     
-    for (p = l->head; p != NULL; p = p->next) printf("%d\n", p->info);
+    for (p = l->head; p != NULL; p = p->next)
+         printf("%d\n", p->info);
+
 }
 int main()
 {
     List *list = new_list();
-    
-    if (list->nelements == 0)
-    {
-        insert_firstelement(list, 2);    
-    }
-    print(list);
-    //insert_inbegin(list, 1);
-    //print(list);
+
+    insert_inbegin(list, 1);
+    insert_inbegin(list, 0);
+    insert_inbegin(list, -1);
+    insert_inend(list, 2);
     insert_inend(list, 3);
     print(list);
-    
-    
+    printf("Length: %d\n", list->nelements);
+    remove_oneelement(list, -1);
+    print(list);
+    printf("Length: %d\n", list->nelements);
+    remove_oneelement(list, 1);
+    print(list);
+    printf("Length: %d\n", list->nelements);
+    remove_oneelement(list, 1);
+    print(list);
+    printf("Length: %d\n", list->nelements);
+    remove_oneelement(list, 3);
+    print(list);
+    printf("Length: %d\n", list->nelements);
+    remove_oneelement(list, 0);
+    print(list);
+    remove_oneelement(list, 2);
+    print(list);
+
     return 0;
 } 
