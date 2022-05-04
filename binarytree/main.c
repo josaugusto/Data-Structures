d6973ce029c4d4845b57a2565d0d708827bba6a8
José Augusto <79456742+josaugusto@users.noreply.github.com>
2022-05-04 09:02:45 -0300

    Update main.c

--- binarytree/main.c
+++ binarytree/main.c
@@ -5,31 +5,78 @@
 typedef struct node
 {
     int info;
-    struct node left;
-    struct node right; 
+    struct node *left;
+    struct node *right; 
 
 }Node;
 
-typedef struct tree
-{
-    Node *root;
-}Tree;
 
-Tree *new_tree() 
+Node *new_node(int value)
 {
-    Tree *t = (Tree*) malloc(sizeof(Tree));
-    t->root = NULL;
-    return t;
+    Node *new = (Node*) malloc(sizeof(Node));
+    new->info = value;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+};
+
+int insert_left(Node *root, int value)
+{
+    if (root->left == NULL)
+    {
+        root->left = new_node(value);
+        root->left->info = value;
+        root->left->left = NULL; root->left->right = NULL;
+        return 1;
+    }
+    else return insert_left(root->left, value);
+
+    return 0;
 }
 
-void insert_node(Tree t, int value)
+int insert_right(Node *root, int value)
 {
+    if (root->right == NULL)
+    {
+        root->right = new_node(value);
+        root->right->info = value;
+        root->right->right = NULL; root->right->left = NULL;
+        return 1;
+    }
+    else return insert_left(root->right, value);
+    
+    return 0;
+}
+
+/*
+void print(Node *root)
+{
+    if (root->left == NULL && root->right == NULL)
+    {
+        printf("%d\n", root->info);
+        return;
+    }
     
 }
+*/
 
 int main()
 {
-    Tree *tree = new_tree();
+
+    Node *root = new_node(1); 
+
+    insert_left(root, 2); 
+    insert_right(root, 3);
+    insert_left(root, 4); 
+    insert_right(root, 5);
+    insert_left(root, 6); 
+    insert_right(root, 7);
+
+    printf("%d\n", root->info); // sure
+    printf("%d\n", root->left->info); // sure
+    printf("%d\n", root->right->info); // sure
+    printf("%d\n", root->left->left->info); // sure
+    printf("%d\n", root->left->right->info);
 
 
     return 0;
