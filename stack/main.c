0d365844f1c7b86d20e648fb321ad35e195a3a1a
José Augusto <79456742+josaugusto@users.noreply.github.com>
2022-05-31 17:37:32 -0300

    Update main.c

--- stack/main.c
+++ stack/main.c
@@ -1,1 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+typedef struct node 
+{
+    int info;
+    struct node *next;
+}Node;
+
+typedef struct
+{
+    int topo;
+    Node *firstin;
+}Stack;
+
+Stack *newStack()
+{
+    Stack *s = (Stack*) malloc(sizeof(Stack));
+    s->topo = -1;
+    s->firstin = NULL;
+    return s;
+}
+
+Node *newNode(int value)
+{
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->info = value;
+    node->next = NULL;
+    return node;
+}
+
+void push(Stack *s, int value)
+{
+    if (s->firstin == NULL) s->firstin = newNode(value);
+    else
+    {
+        Node *p = s->firstin;
+        while(p != NULL)
+            p = p->next;
+        p->next = newNode(value);
+        s->topo++;
+    }
+}
+
+void pop(Stack *s, int value)
+{
+    Node *p = s->firstin;
+
+}
+
+void showStack(Stack *s)
+{
+    Node *p = s->firstin;
+
+    while(p != NULL)
+    {
+        printf("%d", p->info);
+        p = p->next;
+    }
+
+}
+
+int main()
+{
+    Stack *stack = newStack();
+
+    push(stack, 4);
+    //push(stack, 5);
+    //push(stack, 7);
+    showStack(stack);
+
+    return 0;
+}
