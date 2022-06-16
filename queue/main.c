224ae618322649a3ab3fafeef3d6bf9529c80af6
José Augusto <79456742+josaugusto@users.noreply.github.com>
2022-06-16 18:14:19 -0300

    Update main.c

--- queue/main.c
+++ queue/main.c
@@ -2,15 +2,60 @@
 #include <stdlib.h>
 
 
-typedef struct queue
+typedef struct Node
 {
     int info;
-    struct queue *next;
+    struct Node *next;
+}Node;
+
+
+typedef struct queue
+{
+    int n;
+    Node *begin;
+
 }Queue;
 
+Node *newNode(int value)
+{
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->info = value;
+    node->next = NULL;
+    return node;
+}
 
+Queue *newQueue()
+{
+    Queue *q = (Queue*) malloc(sizeof(Queue));
+    q->begin = NULL;
+    return q;
+}
+
+void insert()
+{
+
+}
+
+void remove()
+{
+
+}
+
+void showQueue(Queue *q)
+{
+    if (q->begin == NULL)
+    {
+        printf("This queue was empty.");
+        return;
+    }
+    else
+    {
+        
+    }
+}
 
 int main()
 {
-
+    Queue *queue = newQueue();
+    
 }
